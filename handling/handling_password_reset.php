9732f86700ba5136787d39175ec441eff2f4b170
Lê Ngọc Tuấn <lengoctuan2406@gmail.com>
2022-05-29 10:02:54 +0700

    Update

--- handling/handling_password_reset.php
+++ handling/handling_password_reset.php
@@ -12,56 +12,36 @@ if (isset($_POST['reset'])) {
         $ret = mysqli_query($con, "SELECT * FROM accounts WHERE email='$email';");
         $num = mysqli_fetch_array($ret);
         if ($num > 0) {
-            //vào thư mục php.ini trong đường dẫn C:\xampp\php
-            //tìm kiếm 'mail function' và sửa SMTP lại thành email gửi của mình, nhớ bỏ bảo mật 2 lớp trên email
+            //https://fahmidasclassroom.com/how-to-send-email-in-php-using-mail-function-on-windows/
+            ////trong file php.ini
             //SMTP=smtp.gmail.com
             //smtp_port=587
-            //sendmail_from = 19522466@gm.uit.edu.vn
+            //sendmail_from = phamthithuha28022001@gmail.com
+            //sendmail_path = "C:\xampp\sendmail\sendmail.exe -t"
+            //mail.add_x_header=On
+            ////trong file sendmail.ini
             //smtp_server=smtp.gmail.com
-            //auth_username=19522466@gm.uit.edu.vn
-            //auth_password=748159263(Tuan)
             //smtp_port=587
-            //force_sender=19522466@gm.uit.edu.vn
-
-
-
-
-
-
-            include('extension/PHPMailer-master/src/PHPMailer.php');
-            include('extension/PHPMailer-master/src/SMTP.php');
-            include('extension/PHPMailer-master/src/Exception.php');
-            $mail = new PHPMailer\PHPMailer\PHPMailer();
-
-            $mail->IsSMTP();                       // telling the class to use SMTP
-
-            $mail->SMTPDebug = 0;
-// 0 = no output, 1 = errors and messages, 2 = messages only.
-
-            $mail->SMTPAuth = true;                // enable SMTP authentication
-            $mail->SMTPSecure = "tls";              // sets the prefix to the servier
-            $mail->Host = "smtp.gmail.com";        // sets Gmail as the SMTP server
-            $mail->Port = 587;                     // set the SMTP port for the GMAIL
-
-            $mail->Username = "phamthithuha28022001@gmail.com";  // Gmail username
-            $mail->Password = "748159263(Tuan)))";      // Gmail password
-
-            $mail->CharSet = 'windows-1250';
-            $mail->SetFrom('phamthithuha28022001@gmail.com', 'Example.com Information');
-            $mail->Subject = "helo";
-            $mail->ContentType = 'text/plain';
-            $mail->IsHTML(false);
-
-            $mail->Body = "khong có gì";
-// you may also use $mail->Body = file_get_contents('your_mail_template.html');
-
-            $mail->AddAddress($email, 'Recipients Name');
-// you may also use this format $mail->AddAddress ($recipient);
-
-            if (!$mail->Send()) {
-                $error_message = "Mailer Error: " . $mail->ErrorInfo;
+            //smtp_ssl=tls
+            //error_logfile=error.log
+            //debug_logfile=debug.log
+            //auth_username='phamthithuha28022001@gmail.com'
+            //auth_password='748159263(Tuan)))'
+            ////cho phép ứng dụng kém an toàn truy cập vào
+            //https://myaccount.google.com/lesssecureapps
+            //Email address of the receiver
+            $to = $email;
+            //Email subject
+            $subject = "testing mail() function";
+            //Email message
+            $message = "Sending email using mail() function";
+            //Header information
+            $headers = "From: Admin <admin@fahmidasclassroom.com>\r\n";
+            //Send email
+            if (mail($to, $subject, $message, $headers)) {
+                echo "<script>alert('Gửi email thành công!');</script>";
             } else {
-                $error_message = "Successfully sent!";
+                echo "<script>alert('Gửi email không thành công!');</script>";
             }
         } else {
             echo "<script>alert('Email chưa được đăng kí tài khoản!');</script>";
